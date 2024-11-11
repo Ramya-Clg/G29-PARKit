16149b936e147ea34b26dbaeb6a6a43dc7e3b958
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-11 13:40:55 +0530

    add home page route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import {Login} from './components/Login';
+import {Home} from './components/Home';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/login' element={<Login />}  />
+                    <Route path='/' element={<Home />}  />
                 </Routes>
             </BrowserRouter>
         </div >
