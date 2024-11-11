d0c44e056cc862ae8cfb35614570a7c38c950fb5
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-11 16:10:27 +0530

    remove public in file path

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,11 +5,11 @@ export const Home = () =>{
   return (
     <div className='main-container w-[1920px] h-[3000px] bg-[rgba(203,228,222,0.29)] relative overflow-hidden mx-auto my-0'>
       <div className='w-[2516px] h-[1605.498px] relative z-20 mt-[-524px] mr-0 mb-0 ml-[102px]'>
-        <div className='w-[1658px] h-[1604px] bg-[url(../../public/a1549723-0029-4969-9b1f-64de311e62e2.png)] bg-cover bg-no-repeat absolute top-0 left-[858px] z-[1]' />
+        <div className='w-[1658px] h-[1604px] bg-[url(../../a1549723-0029-4969-9b1f-64de311e62e2.png)] bg-cover bg-no-repeat absolute top-0 left-[858px] z-[1]' />
         <div className='w-[101px] h-[101px] absolute top-[575px] left-[4px] overflow-hidden z-20'>
-          <div className='w-[93.04%] h-[90.33%] bg-[url(../../public/b897f237-6948-4668-b134-e354f484e99e.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[3%] left-[3.33%] z-[21]' />
+          <div className='w-[93.04%] h-[90.33%] bg-[url(../../b897f237-6948-4668-b134-e354f484e99e.png)] bg-[length:100%_100%] bg-no-repeat absolute top-[3%] left-[3.33%] z-[21]' />
           <div className='w-[26.92%] h-[21.54%] absolute top-[73.85%] left-[69.23%] overflow-hidden z-[22]'>
-            <div className='w-[27.197px] h-[19.035px] bg-[url(../../public/6fa08a75-15b2-4b06-aab8-a81a3e5f3ef1.png)] bg-[length:100%_100%] bg-no-repeat relative z-[23] mt-[1.36px] mr-0 mb-0 ml-0' />
+            <div className='w-[27.197px] h-[19.035px] bg-[url(../../6fa08a75-15b2-4b06-aab8-a81a3e5f3ef1.png)] bg-[length:100%_100%] bg-no-repeat relative z-[23] mt-[1.36px] mr-0 mb-0 ml-0' />
           </div>
         </div>
         <div className='w-[138px] h-[49px] bg-[rgba(14,131,136,0.01)] rounded-[12px] border-solid border border-[#0e8388] absolute top-[595px] left-[1431px] shadow-[0_4px_29.4px_0_rgba(0,0,0,0.25)] z-[7]'>
@@ -41,7 +41,7 @@ export const Home = () =>{
           A modern and user-friendly parking experience that saves you time and
           stress.
         </span>
-        <div className='w-[669.975px] h-[449.781px] bg-[url(../../public/98dccff4-ffde-477a-b190-95ee15324f8b.png)] bg-cover bg-no-repeat absolute top-[1155.717px] left-[1077px] z-[16]' />
+        <div className='w-[669.975px] h-[449.781px] bg-[url(../../98dccff4-ffde-477a-b190-95ee15324f8b.png)] bg-cover bg-no-repeat absolute top-[1155.717px] left-[1077px] z-[16]' />
         <div className='w-[194px] h-[64px] bg-[rgba(11,86,86,0.17)] rounded-[12px] border-solid border border-[#2e4f4f] absolute top-[1192px] left-[8px] shadow-[0_4px_63.7px_0_rgba(0,0,0,0.25)] z-[12]'>
           <div className='w-[194px] h-[64px] bg-[rgba(11,86,86,0.62)] rounded-[12px] border-solid border border-[#2e4f4f] absolute top-[-1px] left-[-1px] z-[14]'>
             <span className="flex w-[151.254px] h-[30.897px] justify-center items-center font-['Inter'] text-[24px] font-bold leading-[29.045px] text-[#fff] absolute top-[15.034px] left-[23.881px] text-center whitespace-nowrap z-[15]">
@@ -51,7 +51,7 @@ export const Home = () =>{
         </div>
       </div>
       <div className='w-[1491px] h-[627px] relative z-[27] mt-[279.502px] mr-0 mb-0 ml-[172px]'>
-        <div className='w-[857px] h-[627px] bg-[url(../../public/d9bcc876e5c39abb1ba7fcc6e423a14f1c6bc162.png)] bg-cover bg-no-repeat rounded-[63px] absolute top-0 left-0 z-[27]' />
+        <div className='w-[857px] h-[627px] bg-[url(../../d9bcc876e5c39abb1ba7fcc6e423a14f1c6bc162.png)] bg-cover bg-no-repeat rounded-[63px] absolute top-0 left-0 z-[27]' />
         <span className="flex h-[73px] justify-start items-center font-['Inter'] text-[54px] font-bold leading-[65.352px] text-[#2c3333] absolute top-0 left-[933px] text-left whitespace-nowrap z-[26]">
           ABOUT US
         </span>
@@ -68,7 +68,7 @@ export const Home = () =>{
         </span>
       </div>
       <div className='w-[1920px] h-[838px] bg-[#2c3333] relative z-[28] mt-[174px] mr-0 mb-0 ml-0' />
-      <div className='w-[1920px] h-[1080px] bg-[url(../../public/0d039ffe-15da-4b1f-b366-5327b0d2e26b.png)] bg-cover bg-no-repeat absolute top-[1082px] left-0' />
+      <div className='w-[1920px] h-[1080px] bg-[url(../../0d039ffe-15da-4b1f-b366-5327b0d2e26b.png)] bg-cover bg-no-repeat absolute top-[1082px] left-0' />
     </div>
   );
 }
