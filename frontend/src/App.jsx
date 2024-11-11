17d8516cdedcf3d4b74a1c75223557d6597595ca
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-11 15:35:46 +0530

    stash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import {Login} from './components/Login';
-import {Home} from './components/Home';
 
 function App() {
     return (
@@ -9,7 +8,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path='/login' element={<Login />}  />
-                    <Route path='/' element={<Home />}  />
+                    {/* <Route path='/' element={<Home />}  /> */}
                 </Routes>
             </BrowserRouter>
         </div >
