3d7f234c4c23dc3be6842ae3920aedb3af539c80
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-13 18:59:42 +0530

    add feedback form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import {Login} from './components/Login';
+import { Login } from './components/Login';
 import { Home } from './components/Home';
+import { Button } from './components/ui/button';
+import { Feedback } from './components/Feedback';
 
 function App() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home />}  />
-                    <Route path='/login' element={<Login />}  />
-                    {/* <Route path='/' element={<Home />}  /> */}
+                    <Route path='/' element={<Home />} />
+                    <Route path='/login' element={<Login />} />
                 </Routes>
             </BrowserRouter>
+            <Feedback />
         </div >
     );
 }
