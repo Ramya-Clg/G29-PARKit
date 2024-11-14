0201798892f708b30fbe9de14d6b87415b465adc
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-14 09:12:55 +0530

    booking slot adding imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./components/Login";
 import { Home } from "./components/Home";
-import { Button } from "./components/ui/button";
 import { Feedback } from "./components/Feedback";
+import Booking from "./components/Booking";
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="booking" element={<Booking />} />
         </Routes>
       </BrowserRouter>
-      <Feedback />
     </div>
   );
 }
