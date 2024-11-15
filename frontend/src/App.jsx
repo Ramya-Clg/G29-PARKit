b75e6f01265850c81bdbe40a5c2708fd2b37565d
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-15 11:51:02 +0530

    admin page and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,27 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./components/Login";
-import Home from "./components/Home";
-import { Button } from "./components/ui/button";
+import { Home } from "./components/Home";
 import { Feedback } from "./components/Feedback";
 import { Register } from "./components/Register";
 import { Profile } from "./components/Profile";
 import { Admin } from "./components/Admin";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/feedback" element={<Feedback />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/admin" element={<Admin />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
