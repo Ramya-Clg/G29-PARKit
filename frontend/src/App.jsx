d355c6bca1750051222eca0affba6717122acc72
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-15 12:54:12 +0530

    fixed routing and showing navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,42 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation, Navigate } from "react-router-dom";
 import { Login } from "./components/Login";
 import { Feedback } from "./components/Feedback";
 import { SignUp } from "./components/SignUp";
 import { Profile } from "./components/Profile";
 import { Admin } from "./components/Admin";
 import CreditFormMain from "./components/Credit_Form_Main";
-function App() {
-  return (
-    <div>
-        <Navbar></Navbar>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/credit" element={<CreditFormMain />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+
+function RootApp() {
+    const location = useLocation();
+
+    const hideNavbarRoutes = ["/login", "/signup"];
+    const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+
+    return (
+        <div>
+            {showNavbar && <Navbar />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/feedback" element={<Feedback />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/credit" element={<CreditFormMain />} />
+                <Route path="/admin" element={<Admin />} />
+                
+                <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App;
+export default function App() {
+    return (
+        <BrowserRouter>
+            <RootApp />
+        </BrowserRouter>
+    );
+}
