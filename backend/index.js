4c521fc026fcaa6b7d5e33f5f1b5fc195a569b37
Ramya <176913971+Ramya-Clg@users.noreply.github.com>
2024-11-21 01:29:25 +0530

    cors error while deploying

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  }),
+  cors(),
 );
 app.use(express.json());
 
